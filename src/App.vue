a7fb77d97b2d37caae69a43791e9ed27b0fbe348
Amy Roche <amy.roche31@gmail.com>
2019-08-04 14:57:29 -0600

    question box

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,57 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header />
+    <b-container class="bv-example-row">
+      <b-row sm="6">
+        <b-col><QuestionBox 
+          v-if="questions.length"
+          :currentQuestion="questions[index]"
+          :nextQuestion="nextQuestion"/></b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import Header from './components/Header.vue'
+  import QuestionBox from './components/QuestionBox.vue'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  export default {
+    name: 'app',
+    components: {
+      Header,
+      QuestionBox
+    },
+    data() { 
+      return {
+        questions: [],
+        index: 0
+      }
+    },
+    methods: {
+      nextQuestion() {
+        this.index++
+      }
+    },
+    mounted: function() {
+        fetch('https://opentdb.com/api.php?amount=10&category=12&type=multiple', {
+          method: 'get'
+        }).then((response) => {
+          return response.json()
+        }).then((jsonData) => {
+          this.questions = jsonData.results
+        })
+      }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
